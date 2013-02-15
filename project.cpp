2c04034cc1de797005a7252e4d3f5196ec5f2b07
jonaranola <jonaxranola@yahoo.com>
2013-02-15 09:20:02 -0800

    project.cpp

---
+++ project.cpp
@@ -0,0 +1,66 @@
+#include "../include/Project.h"
+
+using namespace std;
+Project::Project()
+{
+    //ctor
+}
+
+Project::~Project()
+{
+    //dtor
+}
+
+void Project::multiples_3_and_5()
+{
+
+}
+
+void Project::even_fibonacce()
+{
+
+}
+void Project:: largest_prime_factor()
+{
+
+}
+
+void Project:: largerst_palindrome_product()
+{
+
+}
+
+
+
+void Project:: smallest_multiple()
+{
+
+}
+
+void Project:: sum_square_diff()
+{
+
+}
+
+void Project::thou1st_prime()
+{
+
+
+}
+
+void Project::largest_product_in_series()
+{
+
+
+}
+
+void Project::special_pytho_triplet()
+{
+
+}
+
+void Project::summation_of_primes()
+{
+
+}
+
